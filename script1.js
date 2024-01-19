b5487e41b289db21fcace380e799040336c9c81d
Venkateshk7 <154896154+venkateshk7moorthy@users.noreply.github.com>
2024-01-19 23:34:12 +0000

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+let obj1 = [{
+"name":"person1",
+"age":"5"}]
+console.log(obj1[0]);
+
+
+let obj2 = [{
+ "age":"5" ,
+ "name":"person1"}];
+ console.log(obj2[0])
+
+
+if(obj1[1] == obj2[1]){
+    console.log("have same properties without order")
+}
+else
+console.log("have not same properties without order")
---
+++ script3.js
@@ -0,0 +1,12 @@
+var req = new XMLHttpRequest();
+req.open("GET", "https://restcountries.com/v2/all");
+req.send();
+req.onload = function(){
+    var res = JSON.parse(req.response);
+console.log(res);
+
+for(a in res){
+   console.log(a, res[a].name, res[a].region, res[a].subregion, res[a].population);
+
+}
+}
